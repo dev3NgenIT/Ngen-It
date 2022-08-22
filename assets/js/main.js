6aac3a64d1575744685c80da531dc10a7fd3d0b1
Joy602 <aminul.islam4@northsouth.edu>
2022-08-22 14:49:13 +0600

    single page

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,38 @@ $('.banner_slider').slick({
     arrows: true,
     autoplay: true
 });
+
+
+$(document).ready(function() {
+    $('.populer_product_slider').slick({
+        dots: true,
+        autoplay: false,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 4,
+        speed: 200,
+
+        centerPadding: '80px',
+        responsive: [{
+                breakpoint: 760,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    items: 1,
+                }
+
+            },
+
+            {
+                breakpoint: 540,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    items: 1,
+                }
+
+            },
+
+        ]
+    });
+});
